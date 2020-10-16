cb0339669e7fd85178ac64824b9344a409ef53a9
Aaron <54789688+flowersa29@users.noreply.github.com>
2020-10-15 22:53:18 -0500

    firstPush

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import LoginForm from "./components/LoginForm"
+import "./App.css"
 
 function App() {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123",
+  }
+  const [user, setUser] = useState({ name: "", email: "" })
+  const [error, setError] = useState("")
+  const Login = (details) => {
+    console.log(details)
+  }
+
+  const LogOut = () => {
+    console.log("logout")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user.email != "" ? (
+        <div className="welcome">
+          <h2>
+            welcome,<span>{user.name}</span>
+          </h2>
+          <button>LogOut</button>
+        </div>
+      ) : (
+        <LoginForm />
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
